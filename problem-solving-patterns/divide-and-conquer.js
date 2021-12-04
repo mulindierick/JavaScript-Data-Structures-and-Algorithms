290c668a2a104caab9bcdf9cfef4583501b36f91
Erick Mulindi <37551785+mulindierick@users.noreply.github.com>
2021-12-05 00:49:06 +0300

    Update divide-and-conquer.js

--- problem-solving-patterns/divide-and-conquer.js
+++ problem-solving-patterns/divide-and-conquer.js
@@ -1,14 +1,1 @@
-// get a large set of data and divide it into a small chunks of data
-// Example 1 - Binary Search
-// Accepts an  array of sorted values. The values must be sorted returns the index of where the value is found
-
-// bad solution ..good method later
-function binarySearch(arr, value) {
-  if (arr.indexOf(value) !== -1) {
-    return arr.indexOf(value);
-  } else {
-    return -1;
-  }
-}
-
-console.log(binarySearch([1, 2, 3, 4], 11));
+// see binary search
