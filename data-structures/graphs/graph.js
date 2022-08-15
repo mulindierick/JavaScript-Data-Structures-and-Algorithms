fbd3bf71588c04d58dc1e66597d16e68a3ecf24a
Erick Mulindi <37551785+mulindierick@users.noreply.github.com>
2022-08-15 01:58:02 -0400

    add graphs

---
+++ data-structures/graphs/graph.js
@@ -0,0 +1,22 @@
+class Graph {
+  constructor() {
+    this.graph = {};
+  }
+  addVertex(v) {
+  }
+  addEdge(v1, v2) {
+    this.graph[v1].push(v2);
+    this.graph[v2].push(v1);
+  }
+  removeEdge(v1, v2) {
+    this.graph[v1] = this.graph[v1].filter((v) => v !== v2);
+    this.graph[v2] = this.graph[v2].filter((v) => v !== v1);
+  }
+  removeVertex(v) {
+    while (this.graph[v].length) {
+      let edge = this.graph[v].pop();
+      this.removeEdge(v, edge);
+    }
+    delete this.graph[v];
+  }
+}
